867b044872ad54560c4d94a95c697ce78aa70ee2
Everton Barth <e.barth.dev@gmail.com>
2025-11-18 23:32:47 -0300

    Global updates

--- js/my-global-scripts.js
+++ js/my-global-scripts.js
@@ -14,3 +14,27 @@ function addGlobalStylesheet() {
   inject();
 }
 addGlobalStylesheet();
+
+function addContactId() {
+  const targets = document.querySelectorAll('.md-form.layout3.mg-bg, .md-form.layout10.mg-bg');
+  let added = false;
+
+  targets.forEach(target => {
+    if (!target.id) {
+      target.id = 'contact';
+      added = true;
+    }
+  });
+
+  return added;
+}
+
+window.addEventListener('load', () => {
+  if (addContactId()) return;
+
+  const interval = setInterval(() => {
+    if (addContactId()) {
+      clearInterval(interval);
+    }
+  }, 300);
+});
